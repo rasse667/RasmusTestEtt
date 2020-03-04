00ebb76139e6c3751ce42b72e7d0f637cb4557f2
rasse667 <rabg19@student.bth.se>
2020-03-04 14:40:04 +0100

    Update servermain.cpp

--- np_assignment1/servermain.cpp
+++ np_assignment1/servermain.cpp
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
-
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdint.h>
 // Included to get the support library
 #include <calcLib.h>
 
-
+#define PORT "9034"
 
 using namespace std;
 
+int initCalcLib(void);
+int randomInt(void);
+double randomFloat(void);
+char* randomType(void);
 
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
-  
+const double ROUNDING = 0.001;
 
+int main(int argc, char* argv[]) {
+
+	if (argc < 2)
+	{
+		printf("Too few arguments\nExpected: <port>");
+		exit(0);
+	}
+
+	initCalcLib();
+
+	struct addrinfo guide, * serverInfo, * p;
+	uint16_t numBytes;
+	uint16_t sockFD;
+	uint16_t listenFD;
+	uint8_t returnValue;
+
+	int socket()
 }
