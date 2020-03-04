0a0be9b73b3636240cc4e476dceea114f7187e20
rasse667 <rabg19@student.bth.se>
2020-03-04 16:28:36 +0100

    Update servermain.cpp

--- np_assignment1/servermain.cpp
+++ np_assignment1/servermain.cpp
@@ -13,6 +13,7 @@
 #include <calcLib.h>
 
 #define PORT "9034"
+#define BUF_SIZE 500
 
 using namespace std;
 
@@ -35,9 +36,53 @@ int main(int argc, char* argv[]) {
 
 	struct addrinfo guide, * serverInfo, * p;
 	uint16_t numBytes;
-	uint16_t sockFD;
+	uint16_t socketFileDescriptor;
 	uint16_t listenFD;
 	uint8_t returnValue;
 
-	int socket()
+	double fval[2];
+
+	double result;
+	double clientResult;
+
+	guide.ai_family = AF_INET;
+	guide.ai_socktype = SOCK_STREAM;
+	guide.ai_flags = AI_PASSIVE;
+
+	// getaddrinfo(IP, Port, Mall, Lagring), kommer få ut -1 om något gick fel, 0 ifall det gick rätt
+
+	if ((returnValue = getaddrinfo(NULL, argv[1], &guide, &serverInfo)) != 0)
+	{
+		// %s = vill ha in en sträng efter ,
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(returnValue));
+		exit(0);
+	}
+
+	for (p = serverInfo; p != NULL; p = p->ai_next)
+	{
+		if ((socketFileDescriptor = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1)
+		{
+			printf("listener: socket: %s\n", gai_strerror(errno));
+			continue;
+		}
+
+		if (bind(sockFD, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockFD);
+			printf("listener error: bind: %s\n", gai_strerror(errno));
+			continue;
+		}
+
+		break;
+	}
+
+
+	// Om socket inte lyckas skapas.
+
+	if (p == NULL)
+	{
+		printf("Could not create socket.");
+		exit(0);
+	}
+
 }
